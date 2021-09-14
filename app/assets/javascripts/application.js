edc2cf8dd9f58f4358de8d864fc2b5a873ed8db4
Mustafa Ilikkan <milikkan@yahoo.com>
2021-09-15 00:16:42 +0300

    Refactor reports javascript files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,4 +17,5 @@
 //= require js.cookie
 //= require browser_timezone_rails/set_time_zone
 //= require timer
+//= require reports
 //= require common
--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -9,33 +9,8 @@ function initDateTimePickers() {
   });
 }
 
-function resetFilters(event) {
-  event.preventDefault();
-  var options = $("#selection option");
-  $("#selection").val(options[0].val);
-  var date = new Date();
-  var datestr =
-    ("0" + date.getDate()).slice(-2) +
-    "." +
-    ("0" + (date.getMonth() + 1)).slice(-2) +
-    "." +
-    date.getFullYear();
-  $("#start").val(datestr);
-  $("#end").val(datestr);
-  $("#find-entries").prop("disabled", true);
-}
-
 $(function () {
   $(document).foundation();
 
   initDateTimePickers();
-
-  $("#reset-filters").on("click", (event) => resetFilters(event));
-  $("#selection").on("change", () => {
-    if ($("#selection :selected").text() == "Select User") {
-      $("#find-entries").prop("disabled", true);
-    } else {
-      $("#find-entries").prop("disabled", false);
-    }
-  });
 });
