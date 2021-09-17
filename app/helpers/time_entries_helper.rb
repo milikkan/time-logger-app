6dfb5f57fb2d031b7895cb9ce624837c874eb0f5
Mustafa Ilikkan <milikkan@yahoo.com>
2021-09-18 01:01:39 +0300

    Refactor time entries helper

--- app/helpers/time_entries_helper.rb
+++ app/helpers/time_entries_helper.rb
@@ -1,14 +1,11 @@
 module TimeEntriesHelper
 
-  def display_time_entry(dt, format)
-    return '' if dt.nil?
-    dt.localtime.to_s(format)
+  def display_time_entry(time_entry, format)
+    time_entry.localtime.to_s(format)
   end
 
-  def show_duration_between(end_time, start_time)
-    return '' if end_time.nil? or start_time.nil?
-    total_seconds = (end_time - start_time).to_i
-    format_duration(calculate_time_units(total_seconds))
+  def display_duration(time_entry)
+    calculate_time_units(time_entry.duration)
   end
 
   def display_comment(time_entry)
@@ -24,13 +21,12 @@ module TimeEntriesHelper
     days, remaining = total_seconds.divmod(24 * 60 * 60)
     hours, remaining = remaining.divmod(60 * 60)
     minutes, seconds = remaining.divmod(60)
-    return days, hours, minutes, seconds
+    days = days.zero? ? '' : '+' << pluralize(days, 'day')
+    format("%s  %s:%s:%s", days, pad(hours), pad(minutes), pad(seconds))
   end
 
-  def format_duration(units)
-    days, hours, minutes, seconds = units
-    days = days.zero? ? '' : '+' << pluralize(days, 'day')
-    "#{days} #{hours.to_s.rjust(2, '0')}:#{minutes.to_s.rjust(2, '0')}:#{seconds.to_s.rjust(2, '0')}"
+  def pad(unit)
+    unit.to_s.rjust(2, '0')
   end
 
 end
--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -13,4 +13,8 @@ class TimeEntry < ApplicationRecord
       end
     end
   end
+
+  def duration
+    (self.end - self.start).to_i
+  end
 end
